458ddf14d96544566512c12e8e9e39a16c999a53
Ihor Bohdanov <bbengamin@gmail.com>
2024-12-19 15:27:41 +0200

    Integrate users data on the main page. Add basic users list. Add user creation form. Add loading states.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, {useState} from "react";
 import './App.css';
+import {useUsers} from "./hooks/useUsers";
+import {User} from "./types/User";
 
 function App() {
+  const emptyUserData: User = {id: '', firstName: "", lastName: ""}
+
+  const [newUser, setNewUser] = useState<User>(emptyUserData);
+
+  const {
+    users,
+    isUsersLoading,
+    createUser,
+    isCreatingUser,
+    isUsersRefetching
+  } = useUsers();
+
+  const handleAddUser = () => {
+    createUser(newUser);
+    setNewUser(emptyUserData);
+  };
+
   return (
     <div className="App">
-      Hello world
+      <div>
+        <h1>Add New User Form</h1>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={newUser.firstName}
+          onChange={(e) => setNewUser({...newUser, firstName: e.target.value})}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={newUser.lastName}
+          onChange={(e) => setNewUser({...newUser, lastName: e.target.value})}
+        />
+        <button onClick={handleAddUser} disabled={isCreatingUser}>
+          {isCreatingUser ? "Adding..." : "Add User"}
+        </button>
+      </div>
+      {isUsersLoading || isUsersRefetching ? (
+        <span>Loading users...</span>
+      ) : (
+        <div>
+          <h1>Users List</h1>
+          <div>
+            {users?.map((user) => (
+              <div key={user.id}>{`${user.firstName} ${user.lastName}`}</div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
